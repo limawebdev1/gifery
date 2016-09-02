c731d9c2f84ca3ee0796125e6c95b9a4cb998b16
Lisa Ma <limawebdev@gmail.com>
2016-09-02 10:32:46 -0600

    message css,js,html validated/linted

--- js/message.js
+++ js/message.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-  Materialize.toast(`Enter a special message!`, 4000);
+  Materialize.toast('Enter a special message!', 4000);
   $('.halp').click(function(){
-    Materialize.toast(`Enter a special message!`, 4000);
-  })
-  var background = localStorage.getItem('background-color')
+    Materialize.toast('Enter a special message!', 4000);
+  });
+  var background = localStorage.getItem('background-color');
   $('html').css('background', background);
   $('.store').append(localStorage.getItem('storedTitle'));
   $('.giffery').append(localStorage.getItem('giffery'));
   $('.rightarr').click(function(){
     localStorage.setItem('message', $('textarea').val());
-  })
-})
+  });
+});
