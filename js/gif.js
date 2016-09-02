6ee0586dfe79421e58d0556da1664905719cf069
Lisa Ma <limawebdev@gmail.com>
2016-09-02 10:30:35 -0600

    gif css,js,html validated/linted

--- js/gif.js
+++ js/gif.js
@@ -1,52 +1,50 @@
-$(document).ready(function(){
-  Materialize.toast(`Search for GIFs and CLICK on them <br/> to add the ones you want to your message!`, 4000);
-  $('.halp').click(function(){
-    Materialize.toast(`Search for GIFs and CLICK on them <br/> to add the ones you want to your message!`, 4000);
-  })
-  var gifArr = [];
-  $('html').css('background', localStorage.getItem('background-color'));
-  $('.store').append(localStorage.getItem('storedTitle'));
-  $('input').keyup(function(event){
-    event.preventDefault();
-    if($('#gifSearch').val() === ''){
-    }else{
-      $('.heading').css('display','block');
-      $('.popGIF').html('');
-      $('.popGIF').css('display','block');
-      var input = $('#gifSearch').val();
-      gifArr = [];
-      $.ajax({
-        method:'GET',
-        url:`https://api.riffsy.com/v1/search?tag=${input}`,
-        dataType: 'json',
-        success: function(data){
-          $('.popGIF').innerHTML = '';
-          var giffies = data.results;
-          for(var i = 0; i < giffies.length; i++){
-            let url = giffies[i].url;
-            gifArr.push(url);
-          }
-          fillGIF();
-        },
-        error: function(err){
-          console.log('SHMERROR', err);
-        }
-      })
+$(document).ready(function() {
+ Materialize.toast('Search for GIFs and CLICK on them <br/> to add the ones you want to your message!', 4000);
+ $('.halp').click(function() {
+  Materialize.toast('Search for GIFs and CLICK on them <br/> to add the ones you want to your message!', 4000);
+ });
+ var gifArr = [];
+ $('html').css('background', localStorage.getItem('background-color'));
+ $('.store').append(localStorage.getItem('storedTitle'));
+ $('input').keyup(function(event) {
+  event.preventDefault();
+  $('.heading').css('display', 'block');
+  $('.popGIF').html('');
+  $('.popGIF').css('display', 'block');
+  var input = $('#gifSearch').val();
+  gifArr = [];
+  $.ajax({
+   method: 'GET',
+   url: `https://api.riffsy.com/v1/search?tag=${input}`,
+   dataType: 'json',
+   success: function(data) {
+    $('.popGIF').innerHTML = '';
+    var giffies = data.results;
+    for (var i = 0; i < giffies.length; i++) {
+     let url = giffies[i].url;
+     gifArr.push(url);
     }
-  })
-  function fillGIF(){
-    for(var i = 0; i < gifArr.length; i++){
-      var childElem = document.createElement('img');
-      childElem.src = gifArr[i];
-      $('.popGIF').append(childElem);
-    }
+    fillGIF();
+   },
+   error: function(err) {
+    console.log('SHMERROR', err);
+   }
+ });
+ });
+
+ function fillGIF() {
+  for (var i = 0; i < gifArr.length; i++) {
+   var childElem = document.createElement('img');
+   childElem.src = gifArr[i];
+   $('.popGIF').append(childElem);
   }
-  $('.popGIF').on('click','img', function(){
-    var img = document.createElement('img');
-    img.src = this.src;
-    $('.giffage').append(img);
-  })
-  $('.rightarr').click(function(){
-    localStorage.setItem('giffery', $('.giffery').html())
-  })
+ }
+ $('.popGIF').on('click', 'img', function() {
+  var img = document.createElement('img');
+  img.src = this.src;
+  $('.giffage').append(img);
+});
+ $('.rightarr').click(function() {
+  localStorage.setItem('giffery', $('.giffery').html());
+});
 });
