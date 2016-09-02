71ba772f65ceb497aab0c9ee35b882b370646bb9
Lisa Ma <limawebdev@gmail.com>
2016-09-02 10:19:07 -0600

    background js,css,html linted/validated

--- js/background.js
+++ js/background.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-  Materialize.toast(`Select a COLOR or PATTERN! <br/> Hit the RIGHT arrow button to go to the next step!`, 4500);
-  $('.colors').children().each(function(event){
+  Materialize.toast('Select a COLOR or PATTERN! <br/> Hit the RIGHT arrow button to go to the next step!', 4500);
+  $('.colors').children().each(function(){
     $(this).css('background-color', $(this).attr('class'));
-  })
-  $('.colors').on('click', 'div', function(event){
+  });
+  $('.colors').on('click', 'div', function(){
     $('html').css('background',$(this).css('background-color'));
-  })
-  $('.patterns').on('click', 'img', function(event){
-    $('html').css('background', `url(${$(this).attr('src')}) repeat`)
+  });
+  $('.patterns').on('click', 'img', function(){
+    $('html').css('background', `url(${$(this).attr('src')}) repeat`);
     console.log($('html').css('background'));
-  })
+  });
   $('.rightarr').click(function(){
     localStorage.setItem('background-color', $('html').css('background'));
-  })
+  });
   $('.halp').click(function(){
-      Materialize.toast(`Select a COLOR or PATTERN! <br/> Hit the RIGHT arrow button to go to the next step!`, 3000);
-  })
-})
+      Materialize.toast('Select a COLOR or PATTERN! <br/> Hit the RIGHT arrow button to go to the next step!', 3000);
+  });
+});
